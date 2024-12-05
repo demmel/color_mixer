62cce37a4e45421a324dcc51601d9e01a9cee383
David Emmel <dgemmel2@gmail.com>
2024-12-04 21:53:01 -0800

    deploy

--- src/mixbox.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "mixbox" {
-  type ColorArray = [number, number, number];
-
-  type Color = ColorArray | string | { r: number; g: number; b: number };
-
-  function lerp(a: Color, b: Color, t: number): ColorArray;
-}
